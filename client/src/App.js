b13c42f1cfed81dbd0f7c23cdc6458d36f02e129
drdynscript <drdynscript@gmail.com>
2020-04-28 16:17:51 +0200

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Post />
+        <Post
+          title="5 redenen waarom jij moet kiezen voor Grafische en Digitale Media"
+          synopsis="Ben je nog niet volledig overtuigd waarom je moet kiezen voor de opleiding Grafische en Digitale Media aan Arteveldehogeschool? Lees dan snel verder, want derdejaarsstudent Arjan helpt jou de knoop doorhakken."
+          author={{
+            name: "Philippe",
+            picture: "https://scontent-bru2-1.xx.fbcdn.net/v/t1.0-9/42580828_10214673932035654_3017264055002857472_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_oc=AQnltteq4Yyiw4kT7ciu5J0RKUwgtc0ndKi5hWCm4JHAGzCVGCYl9YgTfFMDjEupNwU&_nc_ht=scontent-bru2-1.xx&oh=6e57d7c0bc89a198abfc8c63b6faa79a&oe=5ECCCFAB"
+          }}
+         />
+        <Post
+          title="Herlinde Derycke wint de Febelgra-prijs 2019"
+          synopsis="Studente Herlinde Derycke, afgestudeerd in de afstudeerrichting Grafimediabeleid, wint de Febelgra-prijs 2019. Die is uitgereikt op de proclamatieviering 5 oktober 2019 in Gent. Febelgra, de beroepsvereniging van de Belgische grafische industrie, reikt elk jaar de Febelgra-prijs uit aan een student wie uitzonderlijk werk heeft geleverd tijdens zijn of haar studie en uitblonk als ambassadeur is voor de grafische sector."
+          author={{
+            name: "Lisa",
+            picture: "https://dl.airtable.com/.attachments/8216307230f675f3fa6eb2aa90474c67/7ff726d6/lisa.jfif"
+          }}
+         />
         <Message msg="Greetings Earthling. I am Borg!" createdAt={new Date().toUTCString()} />
         <Person fullName="Philippe" age={37} length={1.72} weight={54} />
         <Person fullName="Lester" age={5} length={1.08} weight={16.8}  />
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <article className="post">
-      <h1>TITEL</h1>
-      <div className="post__synopsis">SYNOPSIS</div>
+      <h1>{props.title}</h1>
+      <div className="post__synopsis">{props.synopsis}</div>
       <div className="post__body">BODY</div>
       <div className="post__author">
         <picture className="picture">
-          <img src="https://scontent-bru2-1.xx.fbcdn.net/v/t1.0-9/42580828_10214673932035654_3017264055002857472_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_oc=AQnltteq4Yyiw4kT7ciu5J0RKUwgtc0ndKi5hWCm4JHAGzCVGCYl9YgTfFMDjEupNwU&_nc_ht=scontent-bru2-1.xx&oh=6e57d7c0bc89a198abfc8c63b6faa79a&oe=5ECCCFAB" alt="Philippe" />
+          <img src={props.author.picture} alt={props.author.name} />
         </picture>
-        <div className="name">Philippe</div>
+        <div className="name">{props.author.name}</div>
       </div>
     </article>
   );
