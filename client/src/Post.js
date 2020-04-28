0680bdfd379458eb9d242bec6b75357e6e7d739e
drdynscript <drdynscript@gmail.com>
2020-04-28 16:41:01 +0200

    WIP

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
+const Author = ({user}) => {
+  return (
+    <div className="post__author">
+      <picture className="picture">
+        <img src={user.picture} alt={user.name} />
+      </picture>
+      <div className="name">{user.name}</div>
+    </div>
+  );
+};
+
 const Post = (props) => {
   return (
     <article className="post">
       <h1>{props.title}</h1>
       <div className="post__synopsis">{props.synopsis}</div>
       <div className="post__body">BODY</div>
-      <div className="post__author">
-        <picture className="picture">
-          <img src={props.author.picture} alt={props.author.name} />
-        </picture>
-        <div className="name">{props.author.name}</div>
-      </div>
+      <Author 
+        user={props.author} 
+      />
     </article>
   );
 };
